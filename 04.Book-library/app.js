7d806714c4070c34d023c1769ae2e6f92e742ab8
Martin <77270844+MartinSimeonovvv@users.noreply.github.com>
2021-07-09 12:13:54 +0300

    Add files via upload

--- 04.Book-library/app.js
+++ 04.Book-library/app.js
@@ -1,4 +1,4 @@
-import { html, render } from 'https://unpkg.com/lit-html?module';
+import { html, render } from 'https://unpkg.com/lit-html?module'; //needed library to create DOM. Learned from Client Side Rendering 
 
 document.getElementById('loadBooks').addEventListener('click', getData)
 document.getElementById('submitForm').addEventListener('submit', postData)
@@ -6,47 +6,73 @@ document.getElementById('submitForm').addEventListener('submit', postData)
 async function getData() {
     const response = await fetch('http://localhost:3030/jsonstore/collections/books')
     const data = await response.json()
-    const values = Object.values(data)
 
+    //using material from Client Side Rendering for creating DOM
     const template = (books) => html`
-        <tr>
-            <td>${books.title}</td>
-            <td>${books.author}</td>
-            <td id=${books._id}>
-                <button @click=${editBtn}>Edit</button>
-                <button @click=${deleteBtn}>Delete</button>
-            </td>
-        </tr>
+    <tr>
+        <td>${books[1].title}</td>
+        <td>${books[1].author}</td>
+        <td id=${books[0]}>
+            <button @click=${edit}>Edit</button>
+            <button @click=${del}>Delete</button>
+        </td>
+    </tr>
     `
-    const tbody = document.querySelector('table tbody')
-    render(values.map(book => template(book)), tbody)
+
+    const tbody = document.querySelector('table>tbody')
+    render(Object.entries(data).map(template), tbody); //append every bookInfo to DOM
 }
 
-async function deleteBtn(event) {
-    const id = event.target.parentElement.id;
+async function postData(e) {
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+    const author = formData.get('author').trim()
+    const title = formData.get('title').trim()
+
+    try {
+        if (!author || !title) {
+            throw new Error('All fields are required!')
+        }
+        await fetch('http://localhost:3030/jsonstore/collections/books', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, author })
+        })
+    } catch (error) {
+        alert('All fields are required!')
+        throw new Error('All fields are required!')
+    }
+
+    e.target.reset()
+    getData()
+}
+
+async function del(e) {
+    const id = e.target.parentElement.id
 
     if (confirm('Are you sure you want to delete?')) {
         await fetch('http://localhost:3030/jsonstore/collections/books/' + id, {
             method: 'delete'
         })
     }
-
     getData()
 }
 
-function editBtn(event) {
-    const id = event.target.parentElement.id;
+async function edit(e) {
+    const id = e.target.parentElement.id
+    const editForm = document.getElementById('editForm')
+    const submitForm = document.getElementById('submitForm');
 
-    const form = document.getElementById('submitForm');
-    const editForm = document.getElementById('editForm');
-
-    form.style.display = 'none';
-    editForm.style.display = 'block';
+    editForm.style.display = 'block'
+    submitForm.style.display = 'none'
 
     editForm.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const formData = new FormData(editForm);
-        const [title, author] = [...formData.values()];
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+        const author = formData.get('author').trim()
+        const title = formData.get('title').trim()
 
         await fetch('http://localhost:3030/jsonstore/collections/books/' + id, {
             method: 'PUT',
@@ -54,22 +80,9 @@ function editBtn(event) {
             body: JSON.stringify({ title, author })
         })
 
-        editForm.reset();
-        getData();
-    });
-}
-
-async function postData(event) {
-    event.preventDefault()
-
-    const formData = new FormData(event.target)
-    const [title, author] = Array.from(formData.values())
-
-    await fetch('http://localhost:3030/jsonstore/collections/books', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, author })
-    });
-
-    getData()
+        editForm.style.display = 'none'
+        submitForm.style.display = 'block'
+        e.target.reset()
+        getData()
+    })
 }
